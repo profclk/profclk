1f4ba91dc16aaecd5753961691a94c2ccc93b4d9
profclk <profclk@hotmail.com>
2011-08-13 17:25:45 -0300

    changes

--- gclick_v2.js
+++ gclick_v2.js
@@ -1,73 +1,77 @@
-window.last_click=null;
-window.clicked_interval = null;
-window.id_product = '363';
+if(typeof window.page_loaded == 'undefined'){
 
-window.getActualTime=function(){
+    window.last_click=null;
+    window.clicked_interval = null;
+    window.id_product = '363';
 
-    var time = parseInt($('.seconds_'+id_product).html());
-    if(isNaN(time))
-        return 30;
-    else return time;
-};
+    window.getActualTime=function(){
 
-window.of = ofertar;
-window.ch_call = channel.callbacks['subastas'];
-window.old_ax = jQuery.ajax;
-
-
-
-window.change_ofertar = function(){
-    jQuery.ajax=function(data){if(data!=undefined) return data;else return false;};
-    ofertar = function(id){
-        if(typeof flag != 'undefined' && flag == "up"){
-            alert("FLAG IS ACTIVE");
-        }
-        else{
-            setTimeout(function(id){jQuery.ajax = window.old_ax;window.of(id);ofertar=window.of;},4000,id);
-        }
+        var time = parseInt($('.seconds_'+id_product).html());
+        if(isNaN(time))
+            return 30;
+        else return time;
     };
-};
 
-window.Finalizar = function(){
-    ofertar = of;
-    channel.callbacks['subastas']=ch_call;
-    jQuery.ajax=old_ax;
-    delete window.a;
-    delete window.of;
-    delete window.old_ax;
-    delete window.readyFunction;
-    delete window.getActualTime;
-    delete window.last_click;
-    delete window.clicked_interval;
-    delete window.change_ofertar;
-    delete window.id_product;
-    delete window.Finalizar;
-};
+    window.of = ofertar;
+    window.ch_call = channel.callbacks['subastas'];
+    window.old_ax = jQuery.ajax;
 
-window.readyFunction=function(){
-  window.js_loaded=true;
-  if (typeof flag != 'undefined' && flag =="up"){
-    alert("FLAG IS UP");
-  }else{
-    if(window.js_loaded){
-        channel.callbacks['subastas'] = [function(data){
 
-           window.last_click = JSON.parse(data);
 
-           window.ch_call[0](data,false,false);
-       }];
+    window.change_ofertar = function(){
+        jQuery.ajax=function(data){if(data!=undefined) return data;else return false;};
+        ofertar = function(id){
+            if(typeof flag != 'undefined' && flag == "up"){
+                alert("FLAG IS ACTIVE");
+            }
+            else{
+                setTimeout(function(id){jQuery.ajax = window.old_ax;window.of(id);ofertar=window.of;},4000,id);
+            }
+        };
+    };
 
-       window.clicked_interval = setInterval(function(){
-           if(getActualTime() <= 4)window.change_ofertar();
-           if(last_click != null && last_click.finalizada=='1')window.Finalizar();},500);
-    }
-  }
-};
+    window.Finalizar = function(){
+        ofertar = of;
+        channel.callbacks['subastas']=ch_call;
+        jQuery.ajax=old_ax;
+        delete window.a;
+        delete window.of;
+        delete window.old_ax;
+        delete window.readyFunction;
+        delete window.getActualTime;
+        delete window.last_click;
+        delete window.clicked_interval;
+        delete window.change_ofertar;
+        delete window.id_product;
+        delete window.page_loaded;
+        delete window.Finalizar;
+    };
 
-$(function($){
+    window.readyFunction=function(){
+      if (typeof flag != 'undefined' && flag =="up"){
+        alert("FLAG IS UP");
+      }else{
+        if(typeof window.page_loaded == 'undefined'){
+            channel.callbacks['subastas'] = [function(data){
 
-    window.readyFunction();
-});
+               window.last_click = JSON.parse(data);
+
+               window.ch_call[0](data,false,false);
+           }];
+
+           window.clicked_interval = setInterval(function(){
+               if(getActualTime() <= 4)window.change_ofertar();
+               if(last_click != null && last_click.finalizada=='1')window.Finalizar();},500);
+        }
+      }
+      window.page_loaded=true;
+    };
+
+    $(function($){
+
+        window.readyFunction();
+    });
+}
 
 
 
